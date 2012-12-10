246bc8ab411994fcf42cebebc7ae688d2294b47a
JN-Jones <jones_h1@web.de>
2012-12-10 21:55:03 +0100

    Einstellungen eingefügt

--- inc/plugins/mention.php
+++ inc/plugins/mention.php
@@ -20,6 +20,101 @@ function mention_info()
     );
 }
 
+function mention_install()
+{
+	global $db;
+	$group = array(
+        "title"          => "Einstellungen für Mention",
+        "name"           => "mention",
+        "description"    => "",
+        "disporder"      => "50",
+        "isdefault"      => "0",
+    );
+    $gid = $db->insert_query("settinggroups", $group);
+
+	$setting = array(
+        "name"           => "mention_subject",
+        "title"          => "Titel",
+        "description"    => "Der Titel der versendeten PN. Ersetzunngsmöglichkeiten: siehe unten",
+        "optionscode"    => "text",
+        "value"          => 'Du wurdest in einem Beitrag erwähnt',
+        "disporder"      => '1',
+        "gid"            => (int)$gid,
+    );
+	$db->insert_query("settings", $setting);
+
+	$setting = array(
+        "name"           => "mention_message",
+        "title"          => "Nachricht",
+        "description"    => "Der Inhalt der PN. Ersetzungsmöglichkeiten:<br />
+{fuser} -> Name des benachrichtigten User<br />{fid} -> ID des benachrichtigen User<br />{flink} -> Link zu dem Profil des Users<br />
+{user} -> Name des Users der erwähnt<br />{uid} -> ID des Users der erwähnt<br />{ulink} -> Link zu dem Profil des Users<br />
+{subject} -> Titel des Beitrags<br />{link} -> Link zu dem Beitrag",
+        "optionscode"    => "textarea",
+        "value"          => 'Hallo {fuser},
+
+Wir schreiben dir, weil du von [url={ulink}]{user}[/url] in dem Beitrag [url={link}]{subject}[/url] erwähnt wurdest.
+
+Mit freundlichen Grüßen',
+        "disporder"      => '2',
+        "gid"            => (int)$gid,
+    );
+	$db->insert_query("settings", $setting);
+
+	$setting = array(
+        "name"           => "mention_disable_quote",
+        "title"          => "Deaktiviere Mention in [quote] Tags",
+        "description"    => "Soll das Plugin innerhalb von [quote] Tags reagieren?",
+        "optionscode"    => "yesno",
+		"value"			 => "yes",
+        "disporder"      => '3',
+        "gid"            => (int)$gid,
+    );
+	$db->insert_query("settings", $setting);
+
+	$setting = array(
+        "name"           => "mention_disable_code",
+        "title"          => "Deaktiviere Mention in [code] Tags",
+        "description"    => "Soll das Plugin innerhalb von [code] Tags reagieren?",
+        "optionscode"    => "yesno",
+		"value"			 => "yes",
+        "disporder"      => '4',
+        "gid"            => (int)$gid,
+    );
+	$db->insert_query("settings", $setting);
+
+	$setting = array(
+        "name"           => "mention_disable_php",
+        "title"          => "Deaktiviere Mention in [php] Tags",
+        "description"    => "Soll das Plugin innerhalb von [php] Tags reagieren?",
+        "optionscode"    => "yesno",
+		"value"			 => "yes",
+        "disporder"      => '5',
+        "gid"            => (int)$gid,
+    );
+	$db->insert_query("settings", $setting);
+	rebuild_settings();
+}
+
+function mention_is_installed() {
+	global $db;
+	$query = $db->simple_select("settinggroups", "gid", "name='mention'");
+	if($db->num_rows($query) > 0)
+	    return true;
+	return false;
+}
+
+function mention_uninstall()
+{
+	global $db;
+
+	$query = $db->simple_select("settinggroups", "gid", "name='mention'");
+    $g = $db->fetch_array($query);
+	$db->delete_query("settinggroups", "gid='".$g['gid']."'");
+	$db->delete_query("settings", "gid='".$g['gid']."'");
+	rebuild_settings();
+}
+
 function mention_activate() {}
 
 function mention_deactivate() {}
@@ -29,7 +124,7 @@ function mention_thread()
 	global $new_thread, $thread_info, $info;
 	$new_thread['pid'] = $thread_info['pid'];
 	$info = $new_thread;
-	preg_replace_callback('/@"([^<]+?)"|@([^\s<)]+)/', "mention_filter", $new_thread['message']);
+	mention_start($new_thread['message']);
 }
 
 function mention_post()
@@ -37,12 +132,25 @@ function mention_post()
 	global $post, $postinfo, $info;
 	$post['pid'] = $postinfo['pid'];
 	$info = $post;
-	preg_replace_callback('/@"([^<]+?)"|@([^\s<)]+)/', "mention_filter", $post['message']);
+	mention_start($post['message']);
+}
+
+function mention_start($message)
+{
+	global $mybb;
+	if($mybb->settings['mention_disable_quote'])
+	    $message = preg_replace("#\[quote\](.*?)\[/quote\]#si", "", $message);
+	if($mybb->settings['mention_disable_code'])
+	    $message = preg_replace("#\[code\](.*?)\[/code\]#si", "", $message);
+	if($mybb->settings['mention_disable_php'])
+	    $message = preg_replace("#\[php\](.*?)\[/php\]#si", "", $message);
+
+	preg_replace_callback('/@"([^<]+?)"|@([^\s<)]+)/', "mention_filter", $message);
 }
 
 function mention_filter(array $match)
 {
-	global $db, $info, $mybb;
+	global $db, $info, $mybb, $user;
 	
 	$found = $match[0];
 	$search = $db->escape_string(my_strtolower(substr($found, 1)));
@@ -55,12 +163,8 @@ function mention_filter(array $match)
 		$pmhandler = new PMDataHandler();
 
 		$pm = array(
-			"subject" => "Du wurdest in einem Beitrag erwähnt",
-			"message" => "Hallo {$user['username']},
-
-Wir schreiben dir, weil du von [url={$mybb->settings['bburl']}/member.php?action=profile&uid={$info['uid']}]{$info['username']}[/url] in einem neuen [url={$mybb->settings['bburl']}/showthread.php?pid={$info['pid']}#pid{$info['pid']}]Beitrag[/url] erwähnt wurdest.
-
-Mit freundlichen Grüßen",
+			"subject" => mention_parser($mybb->settings['mention_subject']),
+			"message" => mention_parser($mybb->settings['mention_message']),
 			"icon" => 0,
 			"fromid" => 0,
 		);
@@ -81,5 +185,21 @@ Mit freundlichen Grüßen",
 
 	}
 	return $found;
+}
+
+function mention_parser($message)
+{
+	global $user, $info;
+
+	$message = str_replace("{fuser}", $user['username'], $message);
+	$message = str_replace("{fid}", $user['uid'], $message);
+	$message = str_replace("{flink}", get_profile_link($user['uid']), $message);
+	$message = str_replace("{user}", $info['username'], $message);
+	$message = str_replace("{uid}", $info['uid'], $message);
+	$message = str_replace("{ulink}", get_profile_link($info['uid']), $message);
+	$message = str_replace("{subject}", $info['subject'], $message);
+	$message = str_replace("{link}", get_post_link($info['pid'], $info['tid']), $message);
+	
+	return $message;
 }
 ?>
